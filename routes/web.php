05ec72fbb524949fdc77caa2140eea01d5100094
jayesh parmar <jp44878182@email.com>
2023-09-01 17:37:30 +0530

    middleware error shol

--- routes/web.php
+++ routes/web.php
@@ -27,13 +27,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-require __DIR__.'/auth.php';
-
-Route::controller(UserController::class)->name('brand.')->group(function () {
+Route::controller(UserController::class)->middleware('auth')->name('brand.')->group(function () {
     Route::get('brands', 'index')->name('brands');
     Route::get('brands-add', 'addBrand')->name('add');
     Route::post('brand-create', 'store')->name('store');
     Route::get('edit-brand/{brandId}', 'edit')->name('edit');
-    Route::post('update-brand/{brandId}', 'update')->name('update'); 
-})->middleware(['auth', 'verified'])->name('dashboard');
+    Route::post('update-brand/{brandId}', 'update')->name('update');
+});
+
+require __DIR__.'/auth.php';
+
