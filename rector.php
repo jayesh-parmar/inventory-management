88ceaddd46f46c9b6f93d59a2c0617f5494db8b0
jayesh parmar <jp44878182@email.com>
2023-09-01 09:50:48 +0530

    add more sets

--- rector.php
+++ rector.php
@@ -8,6 +8,7 @@ use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use RectorLaravel\Set\LaravelSetList;
 
+
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__ . '/app',
@@ -23,6 +24,27 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::CODE_QUALITY,
         LevelSetList::UP_TO_PHP_82,
         LaravelSetList::LARAVEL_100,
+        SetList::PHP_70,
+        SetList::PHP_71,
+        SetList::PHP_72,
+        SetList::PHP_73,
+        SetList::PHP_74,
+        SetList::PHP_80,
+        SetList::PHP_81,
+        SetList::PHP_82,
+        SetList::NAMING,
+        SetList::PHP_52,
+        SetList::PHP_53,
+        SetList::PHP_54,
+        SetList::PHP_55,
+        SetList::PHP_56,
+        SetList::PRIVATIZATION,
+        SetList::TYPE_DECLARATION,
+        SetList::EARLY_RETURN,
+        SetList::INSTANCEOF,
+        SetList::GMAGICK_TO_IMAGICK,
+        SetList::CODING_STYLE,
+        SetList::STRICT_BOOLEANS,    
     ]);
 
     $rectorConfig->rule(RedirectRouteToToRouteHelperRector::class);
