166f7d6099f5c355487431af73e5aecf9d0291ef
jayesh parmar <jp44878182@email.com>
2023-09-01 17:23:04 +0530

    changes done

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-
 Route::get('/', function () {
     return view('auth.login');
 });
