096b6ac351142b8c5b39b29ed747045ecbf20a14
jayesh parmar <jp44878182@email.com>
2023-09-12 17:12:07 +0530

    ma

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::controller(BrandController::class)->middleware('auth')->name('brand.')->g
     Route::post('brands', 'store')->name('store');
     Route::get('brands/{brandId}/edit', 'edit')->name('edit');
     Route::post('brands/{brandId}/update', 'update')->name('update');
+});
 
 Route::controller(SizeController::class)->middleware('auth')->name('size.')->group(function () {
     Route::get('sizes', 'index')->name('index');
