c6ea4d621b0e739ca5cd2c618c175227ee3fdc8d
jayesh parmar <jp44878182@email.com>
2023-09-28 11:30:36 +0530

    change in subcategories blade file

--- resources/views/admin/pages/category/subcategory.blade.php
+++ resources/views/admin/pages/category/subcategory.blade.php
@@ -1,10 +1,9 @@
 <li class="category-item">
     <div style="display: flex; align-items: center; background-color: #f0f0f0;">
-        <p style="margin-right: 10px;">({{ $category->children->count() }}) </p>
+        <h5 style="margin-right: 10px;">({{ $category->children->count() }}) </h5>
         <h5 style="margin-right: 10px;">{{ $category->name }}</h5>
-        <div style="margin-right: 10px;">{{ $category->description }}</div>
-        <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-warning" style="margin-right: 10px;text-decoration: none;">Edit</a>
         <a href="{{ route('categories.create', ['parentId' => $category->id]) }}" style="margin-right: 10px; text-decoration: none;">Add Child Category</a>
+        <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-warning" style="margin-right: 10px;text-decoration: none;">Edit</a>
         <form action="{{ route('categories.destroy', $category->id) }}" method="POST">
             @csrf
             <button type="submit" style="border: none; color: red; background: none; cursor: pointer;" onclick="return confirm('Are you sure you want to delete this category?')">Delete</button>
