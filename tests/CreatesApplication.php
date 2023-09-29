054e95d9380a38d055ef68a6fdfce16f9548560e
jayesh parmar <jp44878182@email.com>
2023-09-29 11:30:40 +0530

    add space in create pplication file

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -4,6 +4,7 @@ namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Application;
+
 trait CreatesApplication
 {
     public function createApplication(): Application
