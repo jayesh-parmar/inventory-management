1b35671dc27caf8d3c89a33a0573a75409f44429
jayesh parmar <jp44878182@email.com>
2023-09-29 15:53:44 +0530

    change in brabd  test file

--- tests/Feature/BrandControllerTest.php
+++ tests/Feature/BrandControllerTest.php
@@ -6,12 +6,11 @@ it('user can add a new brand', function () {
 
     userLogin();
 
-    $response = $this->post(route('brand.store'), [
+    $this->post(route('brand.store'), [
         'name' => 'test',
-    ]);
-    
-    $response->assertStatus(302);
-    $response->assertRedirect(route('brand.index'));
+    ])
+    ->assertStatus(302)
+    ->assertRedirect(route('brand.index'));
 
     $this->assertDatabaseHas('brands', [
         'name' => 'test',
@@ -24,10 +23,10 @@ it('user can update a brand', function () {
 
     $brand = Brand::where('name', 'test')->first();
 
-    $response = $this->post(route('brand.update', $brand->id), [
+    $this->post(route('brand.update', $brand->id), [
         'name' => 'update test',
-    ]);
-    $response->assertRedirect(route('brand.index'));
+    ])
+    ->assertRedirect(route('brand.index'));
 
     $this->assertDatabaseHas('brands', [
         'name' => 'update test',
