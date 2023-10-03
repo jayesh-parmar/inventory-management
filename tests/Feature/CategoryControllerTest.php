4b324ac60b106553351f3427697d6a7ff98c0759
jayesh parmar <jp44878182@email.com>
2023-10-03 16:39:42 +0530

    create category_test

--- tests/Feature/CategoryControllerTest.php
+++ tests/Feature/CategoryControllerTest.php
@@ -7,13 +7,13 @@ it('user can add a new categories', function () {
     userLogin();
 
     $this->post(route('categories.store'), [
-        'name' => 'test1',
+        'name' => 'apple',
     ])
         ->assertStatus(302)
         ->assertRedirect(route('categories.index'));
 
     $this->assertDatabaseHas('categories', [
-        'name' => 'test1',
+        'name' => 'apple',
     ]);
 });
 
@@ -21,15 +21,29 @@ it('user can update a categories', function () {
 
     userLogin();
 
-    $categories = Category::factory()->create(['name' => 'test']);
+    $categories = Category::factory()->create(['name' => 'apple11']);
 
     $this->post(route('categories.update', $categories->id), [
         'id' => $categories->id,
-        'name' => 'update test'
+        'name' => 'update apple11'
     ])
         ->assertRedirect(route('categories.index'));
 
     $this->assertDatabaseHas('categories', [
-        'name' => 'update test',
+        'id' => $categories->id,
+        'name' => 'update apple11',
     ]);
+});
+
+it('user can delete a categories', function () {
+
+    userLogin();
+
+    $categories = Category::factory()->create(['id' => 1, 'name' => 'apple11']);
+
+    $this->post(route('categories.destroy', $categories->id))
+        ->assertStatus(302)
+        ->assertRedirect(route('categories.index'));
+
+    $this->assertDatabaseMissing('categories', ['id' => $categories->id]);
 });
