78192c4f6e8e64fbdb2785f8f43ece424112e1c7
jayesh parmar <jp44878182@email.com>
2023-10-03 10:24:32 +0530

    change in update test

--- tests/Feature/ColorControllerTest.php
+++ tests/Feature/ColorControllerTest.php
@@ -30,7 +30,7 @@ it('user can update a color', function () {
         ->assertRedirect(route('color.index'));
 
     $this->assertDatabaseHas('colors', [
-    'id' => $color->id,
+        'id' => $color->id,
         'name' => 'update red',
     ]);
 });
