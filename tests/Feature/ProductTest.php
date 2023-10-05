06b484452d35a792d306cc1ce052391fd88c375c
jayesh parmar <jp44878182@email.com>
2023-10-05 11:12:15 +0530

    create test function for requred filds missing

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -34,22 +34,44 @@ it('user can update a product', function () {
         'size_id' => Size::factory()->create()->id,
         'color_id' => Color::factory()->create()->id,
         'status' => true,]);
-    
-    $this->post(route('product.update', $productData->id), [
+
+    $updateProductData = [
         'name' => 'PDU Update',
         'brand_id' => $productData->brand_id,
         'size_id' => $productData->size_id,
         'color_id' => $productData->color_id,
         'status' => false,
-    ]  )
+    ];    
+    
+    $this->post(route('product.update', $productData->id), $updateProductData)
         ->assertStatus(302)
         ->assertRedirect(route('product.index'));
 
-    $this->assertDatabaseHas('products', [
-        'name' => 'PDU Update',
+    $this->assertDatabaseHas('products', $updateProductData);
+});
+
+it('user cannot add or update product with missing required fields', function (){
+    
+    userLogin();
+
+    $productData = Product::factory()->create([
+        'name' => 'PDU',
+        'brand_id' => Brand::factory()->create()->id,
+        'size_id' => Size::factory()->create()->id,
+        'color_id' => Color::factory()->create()->id,
+        'status' => true,]);
+
+    $updateProductData = [
+        
         'brand_id' => $productData->brand_id,
         'size_id' => $productData->size_id,
         'color_id' => $productData->color_id,
         'status' => false,
-    ]);
+    ];    
+    
+    $this->post(route('product.update', $productData->id), $updateProductData)
+        ->assertStatus(302)
+        ->assertSessionHasErrors();
+        
+    $this->assertTrue(session('errors')->has('name'), 'The name field is required.');        
 });
