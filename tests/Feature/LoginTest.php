43805b2aba62d150e45d733ca43a159617935a31
jayesh parmar <jp44878182@email.com>
2023-10-05 13:04:54 +0530

    create logout function in login test

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -4,16 +4,16 @@ use App\Models\User;
 
 it('login screen can be rendered', function () {
 
-    $this->get('/login')
+    $this->get(route('login'))
 
     ->assertStatus(200);
 });
 
-it('users can login using the login screen', function () {
+it('users can login and redirected to the dashboard', function () {
 
     $user = User::factory()->create();
 
-    $this->post('/login', [
+    $this->post(route('login'), [
         'email' => $user->email,
         'password' => 'password',
     ])
@@ -26,10 +26,18 @@ it('users can not login with invalid password', function () {
 
     $user = User::factory()->create();
 
-    $this->post('/login', [
+    $this->post(route('login'), [
         'email' => $user->email,
         'password' => 'wrong-password',
     ]);
+
+    $this->assertGuest();
+});
+
+it('users can logout and redirected to the login page', function () {
+
+    $this->post(route('logout'))
+    ->assertRedirect(route('login'));
 
     $this->assertGuest();
 });
