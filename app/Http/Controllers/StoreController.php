92869fbf5990078979b26e2ad820e54abcca7a9d
jayesh parmar <jp44878182@email.com>
2023-10-06 18:29:19 +0530

    create store curd

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreRequest;
+use App\Models\Store;
 
 class StoreController extends Controller
 {
-    //
+    public function index()
+    {
+        $stores = Store::select('id', 'name')->paginate(10);
+
+        return view('admin.pages.store.index', ['stores' => $stores]);
+    }
+
+    public function add()
+    {
+        return view('admin.pages.store.form');
+    }
+
+    public function store(StoreRequest $request)
+    {
+        Store::create($request->validated());
+
+        return redirect()->route('store.index')->with('success', 'store Added successfully.');
+    }
+
+    public function edit(string $storeId)
+    {
+        $store = Store::select('id', 'name')->find($storeId);
+
+        return view('admin.pages.store.form', ['store' => $store]);
+    }
+
+    public function update(StoreRequest $request, Store $store)
+    {
+        $store->update($request->validated());
+
+        return redirect()->route('store.index')->with('success', 'store Update successfully.');
+    }
+
+    public function destroy(string $storeId)
+    {
+        $store = Store::find($storeId);
+        $store->delete();
+        
+        return redirect()->route('store.index')->with('success', 'Store deleted successfully.');
+    }
 }
--- app/Models/Store.php
+++ app/Models/Store.php
@@ -10,6 +10,8 @@ class Store extends Model
 {
     use HasFactory, HasUuids;
 
+    public $timestamps = false;
+    
     protected $fillable = [
         'name',
     ]; 
